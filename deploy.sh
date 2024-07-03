dd02a5c2c91b0949e705a1990196f1cc194adfaa
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-07-03 21:55:35 +0700

    ci(actions): add github action config

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/run/current-system/sw/bin/zsh
+echo "changing dir..."
+cd Fun/Projects/e-bc-tc
+
+echo "pulling from repo..."
+git add .
+git stash
+git pull
+git stash pop
+
+echo "injecting .env..."
+mv env .env
+
+echo "init/installing deps using pnpm..."
+nix develop --command bash -c "pnpm install"
+
+echo "restarting systemd unit..."
+sudo systemctl restart eigen-tc
+
+echo "systemd unit status..."
+sudo systemctl status eigen-tc
+
+echo "done!!!"
