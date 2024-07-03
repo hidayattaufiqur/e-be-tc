cc2c1e03568f3c25ca7120be603c0fb7b0f1e567
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-07-03 15:24:28 +0700

    test(jest): add unit tests for borrow and return book

--- tests/unit-tests/book/handlers/http_handler.spec.ts
+++ tests/unit-tests/book/handlers/http_handler.spec.ts
@@ -2,24 +2,43 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import HttpHandler from '../../../../src/modules/book/handlers/http_handler';
 import { IUsecaseQuery, IUsecaseCommand } from '../../../../src/modules/book/book';
-import { IBook } from '../../../../src/modules/book/models/book';
+import { IUsecaseQuery as IUsecaseMemberQuery, IUsecaseCommand as IUsecaseMemberCommand } from '../../../../src/modules/member/member';
+import { IBook, IBorrowRecord } from '../../../../src/modules/book/models/book';
 
 import { expect, it, describe, jest, beforeEach } from '@jest/globals';
+import { IMember } from '../../../../src/modules/member/models/member';
 
 // Mock dependencies
 const mockUsecaseQuery: jest.Mocked<IUsecaseQuery> = {
   GetBook: jest.fn(),
   GetBooks: jest.fn(),
+  GetBorrowRecords: jest.fn(),
+  GetBorrowRecord: jest.fn(),
+  GetInStockBooks: jest.fn(),
+  GetBookById: jest.fn(),
 };
 
 const mockUsecaseCommand: jest.Mocked<IUsecaseCommand> = {
   AddBook: jest.fn(),
   UpdateBook: jest.fn(),
   RemoveBook: jest.fn(),
+  BorrowBook: jest.fn(),
+  ReturnBook: jest.fn(),
+};
+
+const mockUsecaseMemberQuery: jest.Mocked<IUsecaseMemberQuery> = {
+  GetMember: jest.fn(),
+  GetMemberById: jest.fn(),
+  GetMembers: jest.fn(),
+};
+
+const mockUsecaseMemberCommand: jest.Mocked<IUsecaseMemberCommand> = {
+  AddMember: jest.fn(),
+  UpdateMember: jest.fn(),
 };
 
 // Create instances of HttpHandler with mocked dependencies
-const httpHandler = new HttpHandler(mockUsecaseQuery, mockUsecaseCommand);
+const httpHandler = new HttpHandler(mockUsecaseQuery, mockUsecaseCommand, mockUsecaseMemberQuery, mockUsecaseMemberCommand);
 
 describe('HttpHandler', () => {
   let req: Partial<Request>;
@@ -35,52 +54,149 @@ describe('HttpHandler', () => {
     next = jest.fn();
   });
 
-  describe('getBook', () => {
-    it('should return a book if found', async () => {
+  describe('borrowBook', () => {
+    it('should borrow a book if all conditions are met', async () => {
       const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: null };
+      const borrowRecords: IBorrowRecord[] = [];
+
       mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+      mockUsecaseQuery.GetBorrowRecords.mockResolvedValueOnce(borrowRecords);
 
-      req.params = { code: 'JK-45' };
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
 
-      await httpHandler.getBook(req as Request, res as Response);
+      await httpHandler.borrowBook(req as Request, res as Response);
 
       expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
-      expect(res.send).toHaveBeenCalledWith({
-        message: 'Book retrieved successfully',
-        code: StatusCodes.OK,
-        data: book,
-      });
+      expect(res.send).toHaveBeenCalledWith({ message: 'Book borrowed successfully', code: StatusCodes.OK });
     });
 
-    it('should return 404 if book not found', async () => {
+    it('should return 404 if book is not available', async () => {
       mockUsecaseQuery.GetBook.mockResolvedValueOnce(null);
 
-      req.params = { code: 'JK-45' };
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
 
-      await httpHandler.getBook(req as Request, res as Response);
+      await httpHandler.borrowBook(req as Request, res as Response);
 
       expect(res.status).toHaveBeenCalledWith(StatusCodes.NOT_FOUND);
-      expect(res.send).toHaveBeenCalledWith({
-        message: 'Book not found',
-        code: StatusCodes.NOT_FOUND,
-      });
+      expect(res.send).toHaveBeenCalledWith({ message: 'Stock is not available', code: StatusCodes.NOT_FOUND });
+    });
+
+    it('should return 400 if member is penalized', async () => {
+      const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: new Date() };
+      
+      mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.borrowBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Member is penalized.', code: StatusCodes.BAD_REQUEST });
+    });
+
+    it('should return 400 if member has borrowed more than one book', async () => {
+      const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: null };
+      const borrowRecords: IBorrowRecord[] = [{ bookId: 1, memberId: 1, borrowedTime: new Date(), moreThanSevenDays: false }, { bookId: 2, memberId: 1, borrowedTime: new Date(), moreThanSevenDays: false }];
+
+      mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+      mockUsecaseQuery.GetBorrowRecords.mockResolvedValueOnce(borrowRecords);
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.borrowBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Member has more than one books borrowed.', code: StatusCodes.BAD_REQUEST });
     });
 
     it('should return 500 if there is an error', async () => {
       mockUsecaseQuery.GetBook.mockRejectedValueOnce(new Error('Test Error'));
 
-      req.params = { code: 'JK-45' };
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
 
-      await httpHandler.getBook(req as Request, res as Response);
+      await httpHandler.borrowBook(req as Request, res as Response);
 
       expect(res.status).toHaveBeenCalledWith(StatusCodes.INTERNAL_SERVER_ERROR);
-      expect(res.send).toHaveBeenCalledWith({
-        message: 'Internal server error',
-        code: StatusCodes.INTERNAL_SERVER_ERROR,
-      });
+      expect(res.send).toHaveBeenCalledWith({ message: 'Internal server error', code: StatusCodes.INTERNAL_SERVER_ERROR });
     });
   });
 
-  // TODO: add tests for getBooks, addBook, updateBook, removeBook
-});
+  describe('returnBook', () => {
+    it('should return a book if all conditions are met', async () => {
+      const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: null };
+      const borrowRecord: IBorrowRecord = { bookId: 1, memberId: 1, borrowedTime: new Date(), moreThanSevenDays: false };
 
+      mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+      mockUsecaseQuery.GetBorrowRecord.mockResolvedValueOnce(borrowRecord);
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.returnBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Book returned successfully', code: StatusCodes.OK });
+    });
+
+    it('should return 404 if member has not borrowed the book', async () => {
+      const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: null };
+      
+      mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+      mockUsecaseQuery.GetBorrowRecord.mockResolvedValueOnce(null);
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.returnBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.NOT_FOUND);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Member has not borrowed the book', code: StatusCodes.NOT_FOUND });
+    });
+
+    it('should penalize member if book is returned late', async () => {
+      const book: IBook = { code: 'JK-45', title: 'Harry Potter', author: 'J.K Rowling', stock: 1 };
+      const member: IMember = { code: 'M001', name: 'John Doe', id: 1, penalized_at: null };
+      const borrowRecord: IBorrowRecord = { bookId: 1, memberId: 1, borrowedTime: new Date(new Date().getTime() - 8 * 24 * 60 * 60 * 1000), moreThanSevenDays: true };
+
+      mockUsecaseQuery.GetBook.mockResolvedValueOnce(book);
+      mockUsecaseMemberQuery.GetMember.mockResolvedValueOnce(member);
+      mockUsecaseQuery.GetBorrowRecord.mockResolvedValueOnce(borrowRecord);
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.returnBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Book returned successfully', code: StatusCodes.OK });
+      expect(mockUsecaseMemberCommand.UpdateMember).toHaveBeenCalled();
+    });
+
+    it('should return 500 if there is an error', async () => {
+      mockUsecaseQuery.GetBook.mockRejectedValueOnce(new Error('Test Error'));
+
+      req.params = { bookCode: 'JK-45' };
+      req.body = { memberCode: 'M001' };
+
+      await httpHandler.returnBook(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(res.send).toHaveBeenCalledWith({ message: 'Internal server error', code: StatusCodes.INTERNAL_SERVER_ERROR });
+    });
+  });
+});
